85cd0a5527663b1523f90f4b84af4cb19623283c
evavranici <evavranici@gmail.com>
2020-04-11 22:35:48 +0200

    Exercise 5 Lab 1

---
+++ LabOne/Form5.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form5
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox3.Text = Convert.ToDecimal(TextBox1.Text) - Convert.ToDecimal(TextBox2.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
