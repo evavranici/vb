fe520cb19c2832a7a75544fe880ed966220c444c
evavranici <evavranici@gmail.com>
2020-04-11 22:23:59 +0200

    Exercise 4 Lab 1

---
+++ LabOne/Form4.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form4
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblStreet = New System.Windows.Forms.Label()
+        Me.lblCityStateZip = New System.Windows.Forms.Label()
+        Me.btnShowInfo = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(115, 43)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(0, 19)
+        Me.lblName.TabIndex = 0
+        Me.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblStreet
+        '
+        Me.lblStreet.AutoSize = True
+        Me.lblStreet.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStreet.Location = New System.Drawing.Point(106, 79)
+        Me.lblStreet.Name = "lblStreet"
+        Me.lblStreet.Size = New System.Drawing.Size(0, 19)
+        Me.lblStreet.TabIndex = 1
+        Me.lblStreet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCityStateZip
+        '
+        Me.lblCityStateZip.AutoSize = True
+        Me.lblCityStateZip.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCityStateZip.Location = New System.Drawing.Point(87, 116)
+        Me.lblCityStateZip.Name = "lblCityStateZip"
+        Me.lblCityStateZip.Size = New System.Drawing.Size(0, 19)
+        Me.lblCityStateZip.TabIndex = 2
+        Me.lblCityStateZip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnShowInfo
+        '
+        Me.btnShowInfo.Location = New System.Drawing.Point(70, 191)
+        Me.btnShowInfo.Name = "btnShowInfo"
+        Me.btnShowInfo.Size = New System.Drawing.Size(75, 23)
+        Me.btnShowInfo.TabIndex = 3
+        Me.btnShowInfo.Text = "Show Info"
+        Me.btnShowInfo.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(164, 191)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 260)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnShowInfo)
+        Me.Controls.Add(Me.lblCityStateZip)
+        Me.Controls.Add(Me.lblStreet)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form4"
+        Me.Text = "Name and Address"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblStreet As Label
+    Friend WithEvents lblCityStateZip As Label
+    Friend WithEvents btnShowInfo As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ LabOne/Form4.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form4
+    Private Sub btnShowInfo_Click(sender As Object, e As EventArgs) Handles btnShowInfo.Click
+        lblName.Text = "Eva Vranici"
+        lblStreet.Text = "Dumb Street"
+        lblCityStateZip.Text = "Dumb City, State, ZIP"
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+End Class
