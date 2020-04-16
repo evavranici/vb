e4c4abccf8ba519e4e4f56b41edc831587b829a2
evavranici <evavranici@gmail.com>
2020-04-16 19:36:55 +0200

    E10/13 L1

---
+++ LabOne/Form13.vb
@@ -0,0 +1,21 @@
+﻿' Name: Exercise 13 Lab 1
+' Description:
+' Programmer: Eva Vranici
+
+Public Class Form13
+    Private Sub btnShowDate_Click(sender As Object, e As EventArgs) Handles btnShowDate.Click
+        lblDateString.Text = txtDayOfWeek.Text + ", " + txtMonth.Text + txtDayOfMonth.Text + ", " + txtYear.Text
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtMonth.Text = ""
+        txtDayOfMonth.Text = ""
+        txtDayOfWeek.Text = ""
+        txtYear.Text = ""
+        lblDateString.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+End Class
---
+++ LabOne/Form16.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form16
+    Private Sub Form16_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = Now.AddDays(6).ToString("dd MMMM yyyy")
+        TextBox2.Text = TimeOfDay.ToString("hh:mm:ss")
+    End Sub
+End Class
