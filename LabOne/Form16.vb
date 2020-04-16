54e88d561e1732917441e316f672e2ef7a5e0968
evavranici <evavranici@gmail.com>
2020-04-16 19:56:49 +0200

    E16/17 L1

--- LabOne/Form16.vb
+++ LabOne/Form16.vb
@@ -1,4 +1,8 @@
-﻿Public Class Form16
+﻿' Name: Exercise 16 Lab 1
+' Description: not completed, only imported part is coded
+' Programmer: Eva Vranici
+
+Public Class Form16
     Private Sub Form16_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TextBox1.Text = Now.AddDays(6).ToString("dd MMMM yyyy")
         TextBox2.Text = TimeOfDay.ToString("hh:mm:ss")
---
+++ LabOne/Form17.vb
@@ -0,0 +1,9 @@
+﻿' Name: Exercise 17 Lab 1
+' Description: not completed, only imported part is coded
+' Programmer: Eva Vranici
+
+Public Class Form17
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        MessageBox.Show("One")
+    End Sub
+End Class
